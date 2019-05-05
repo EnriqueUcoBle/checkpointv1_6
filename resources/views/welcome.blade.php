1510208362e730469efc9a42f1ba1645a939f390
EnriqueUcoBle <mhanuelxd.03@gmail.com>
2019-05-05 01:30:25 -0500

    nuevo archivo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CheckPoint  |  V_1.6</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- styles -->
+    <link href="css/styles.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Plantel 28</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">CheckPoint | V_1.6<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Acceder</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#" >Registrarme</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Que es CheckPoint?</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+	                
+	 
+
+    <div class="page-content">  
+		  	<div class="row" >
+		  		<div class="col-md-12 panel-warning">
+		  			<div class="content-box-header panel-heading">
+	  					<div class="panel-title ">Para Los Padres de Familia</div>
+						
+						<div class="panel-options">
+							<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+							<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+						</div>
+		  			</div>
+						
+		  			<div class="content-box-large box-with-header"><h1>CheckPoint </h1>
+
+						<h3>un sistema pensado para el control de la informacion de sus hijos sobre el ambito academico dentro del plantel 28</h3>
+
+					</div>
+		  		</div>
+		  	</div>
+				
+		</div>
+    
+
+    <footer style="bottom:0;">
+         <div class="container">
+         
+            <div class="copy text-center">
+               CheckPoint Plantel 28 Beta      <a href='#'>Declaracion de Privacidad</a>
+            </div>
+            
+         </div>
+      </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
